dc0cf17b9f013d5790fec0590bac1f67a8fafbb4
Nicholas Badiola <NicholasBadiola@gmail.com>
2022-01-31 14:24:19 -0500

    finished all iterations

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+    constructor() {
+        this.items = [];
+        this.length = 0;
+    }
 
-  add(item) {}
+    add(item) {
+        this.items.push(item);
+        this.items.sort((a, b) => a - b);
+        this.length++;
+    }
 
-  get(pos) {}
+    get(pos) {
+        if (this.items.indexOf(pos) === -1) {
+            throw new Error("OutOfBounds");
+        }
+        return this.items[pos];
+    }
 
-  max() {}
+    max() {
+        if (this.items.length === 0) {
+            throw new Error("EmptySortedList");
+        }
+        return Math.max(...this.items);
+    }
 
-  min() {}
+    min() {
+        if (this.items.length === 0) {
+            throw new Error("EmptySortedList");
+        }
+        return Math.min(...this.items);
+    }
+    sum() {
+        if (this.items.length === 0) {
+            return 0;
+        }
+        return this.items.reduce(function(a, b) {
+            return a + b;
+        });
+    }
 
-  sum() {}
-
-  avg() {}
+    avg() {
+        if (this.items.length === 0) {
+            throw new Error("EmptySortedList");
+        }
+        return this.sum() / this.items.length;
+    }
 }
 
 module.exports = SortedList;
